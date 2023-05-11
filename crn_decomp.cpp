0591e9251f8a0b6e75324c1e1585ef47881db2a7
meche234 <102772603+meche234@users.noreply.github.com>
2023-05-11 03:26:36 -0400

    Update crn_decomp.cpp

--- crn_decomp.cpp
+++ crn_decomp.cpp
@@ -2,18 +2,17 @@
 
 #include <string.h>
 
-#ifdef __APPLE__
-    #include <malloc/malloc.h>
-    #define malloc_usable_size malloc_size
+#if defined(__APPLE__)
+#define malloc_usable_size malloc_size
 #else
-    #ifdef __FreeBSD__
-        #include <stdlib.h>
-    #else
-        #include <malloc.h>
-    #endif
-    #ifdef _WIN32
-        #define malloc_usable_size _msize
-    #endif
+#if defined(__FreeBSD__)
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#endif
+#ifdef _WIN32
+#define malloc_usable_size _msize
+#endif
 #endif
 
 #include "crn_decomp.h"
